40d5836806979378cec25229221ae21b4a718979
Balamurugan.Balasubramanian <kbcbala@gmail.com>
2022-02-12 23:33:34 +0000

    added the project files

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 variable "profile" {
   description = "Profile with permissions to provision the AWS resources."
-  default     = "beld"
+  default     = "bala"
 }
 
 variable "region" {
   description = "Region to provision the resources into."
-  default     = "sa-east-1"
+  default     = "eu-west-2"
 }
 
 provider "aws" {
@@ -71,6 +71,24 @@ resource "aws_instance" "inst1" {
   vpc_security_group_ids = [
     "${aws_security_group.allow-ssh-and-egress.id}",
   ]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    export CIRCLECI_TOKEN=a4f962734f21b52bb377555a4f503e966194483f
+    echo test of user_data | sudo tee /tmp/user_data.log
+    curl -u ${CIRCLECI_TOKEN}: -X POST --header "Content-Type: application/json" -d '{ 
+      "branch": "develop", 
+      "parameters": { 
+      "destroy_test_dev": true, 
+      "run_infra_build": false
+      } 
+    }' https://circleci.com/api/v2/project/gh/kbcbals/circleci-lambda/pipeline
+
+  EOF
+
+
+
+
 }
 
 /* resource "aws_instance" "inst2" {
